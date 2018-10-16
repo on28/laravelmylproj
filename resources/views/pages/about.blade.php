80c9f8e39dc1dcd7abb6e94f2f6ac9102c51a3da
Oliver <on28@njit.edu>
2018-10-16 16:36:22 -0400

    Feature: Added the contacts page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,10 +3,12 @@
 @section('content')
 
 
-    <small>It's Nice to Meet You!</small>
-    </h1>
-    <p>Welcome to our about page. All you need to know is that we are all about learning everything about laravel!</p>
+    <div class="container">
 
+        <h1><b>Welcome to our about page.</b></h1>
+        <p>It's Nice to Meet You! <br>All you need to know is that we are all about learning everything about laravel!</p>
+
+    </div>
 
 
 @endsection
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+    <div class="container">
+
+        <h1><b>Welcome to our about Contact Page.</b></h1>
+        <p>Please see the form and phone number on this site to contact the site owner for sponsorship <br> or if you have any questions about laravel</p>
+
+    </div>
+
+
+@endsection
