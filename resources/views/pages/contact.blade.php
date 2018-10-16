247611ad17018c4db45816d0931d735094e9b10e
Oliver <on28@njit.edu>
2018-10-16 18:00:18 -0400

    Feature: added padding and realigned data in the contacts box

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,24 +2,23 @@
 
 @section('content')
 
-
-
-        <h1><b>Welcome to our about Contact Page.</b></h1>
-        <p>Please use the form and phone number on this site to contact the site owner for sponsorship <br> or if you have any questions about laravel</p>
-
-        <form action="/contact" method="post">
-            {{ csrf_field() }}
-            <div class="form-group">
-                <label for="email">Email address</label>
-                <input name="email" type="email" class="form-control" id="email" placeholder="yourname@domain.com">
-            </div>
-
-            <div class="form-group">
-                <label for="body">Please enter your message below</label>
-                <textarea name="body" class="form-control" id="body" rows="3"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary mb-2">Submit</button>
-        </form>
-
-
+    <div class="card mt - 3 pl-2 pr-2">
+        <div class="card-title"><h1><b>Welcome to our Contact Page.</b></h1>
+            <p class="" lead">Please use the form and phone number on this site to contact the site owner for
+            sponsorship <br> or if you have any questions about laravel</p></div>
+        <div class="card-body">
+            <form action="/contact" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input name="email" type="email" class="form-control" id="email" placeholder="yourname@domain.com">
+                </div>
+                <div class="form-group">
+                    <label for="body">Please enter your message below</label>
+                    <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            </form>
+        </div>
+    </div>
 @endsection
