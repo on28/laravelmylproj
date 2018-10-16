c5974be2ccd1849e8c4723feae084da93e3070fb
Oliver <on28@njit.edu>
2018-10-16 15:11:09 -0400

    Feature: Added index page and updated web.php with an additonal route.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,12 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+
+Route::get('/contact', function () {
+    return view('about');
+});
+
+Route::post('/about', function () {
+    return view('about');
+});
